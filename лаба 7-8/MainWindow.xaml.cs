3a344989bc1d695e91e425c86748c73c1c4a5921
Георгий Дудырин <gosha84gur@outlook.com>
2022-10-28 14:18:19 +0300

    NEw

--- лаба 7-8/MainWindow.xaml.cs
+++ лаба 7-8/MainWindow.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+///
+////
 namespace лаба_7_8
 {
     /// <summary>
